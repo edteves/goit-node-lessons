846bb97ecdcdb17b1e4b8672d25ce253c45dc627
Marvin Pacis <108392678+marventures@users.noreply.github.com>
2024-04-16 12:00:41 +0800

    Update code

--- starter/routes/api/contactsRouter.js
+++ starter/routes/api/contactsRouter.js
@@ -4,23 +4,43 @@ import { mockData } from "../../models/mockData.js";
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  //LOGIC HERE
+  try {
+    //LOGIC HERE
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  //LOGIC HERE
+  try {
+    //LOGIC HERE
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/", async (req, res, next) => {
-  //LOGIC HERE
+  try {
+    //LOGIC HERE
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  //LOGIC HERE
+  try {
+    //LOGIC HERE
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  //LOGIC HERE
+  try {
+    //LOGIC HERE
+  } catch (error) {
+    next(error);
+  }
 });
 
 export { router };
